e61c533959c943df39201ab337c28fa1c9673810
Aman34772 <amankumar34772@gmail.com>
2024-05-17 10:28:34 +0530

    first commit

---
+++ schema.js
@@ -0,0 +1,25 @@
+const mongoose = require("mongoose");
+const { Schema } = mongoose;
+
+const movieSchema = new mongoose.Schema({
+  movie: {
+    type: String,
+    required: true,
+  },
+  seats: {
+    type: Object,
+    required: true,
+  },
+  slot: {
+    type: String,
+    required: true,
+  },
+  date_created: {
+    type: Date,
+    default: Date.now,
+  },
+});
+
+const Movie = mongoose.model("Movie", movieSchema);
+
+module.exports = Movie;
